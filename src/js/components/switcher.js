29e197be6b55e5976070aea275c1e57c97ddc71c
AnnaYegorova <good4anna@gmail.com>
2022-01-30 17:05:12 +0200

    create switcher in section genres

--- src/js/components/switcher.js
+++ src/js/components/switcher.js
@@ -1,9 +1,7 @@
 const checkboxBtn = document.querySelector('#theme-switch-toggle');
 console.log(checkboxBtn);
 const body = document.querySelector('body');
-const gallery = document.querySelector('.gallery');
 const footer = document.querySelector('.footer');
-const pagination = document.querySelector('.pagination-theme');
 
 const Theme = {
   LIGHT: 'light-theme',
@@ -17,26 +15,18 @@ checkboxBtn.addEventListener('change', changeTheme);
 function changeTheme(e) {
   const theme = e.target.checked;
   if (theme) {
-    gallery.classList.add(DARK);
-    pagination.classList.add(DARK);
-
+    body.classList.add(DARK);
     footer.classList.add(DARK);
 
-    gallery.classList.remove(LIGHT);
-    pagination.classList.remove(LIGHT);
-
+    body.classList.remove(LIGHT);
     footer.classList.remove(LIGHT);
 
     localStorage.setItem('theme', DARK);
   } else {
-    gallery.classList.add(LIGHT);
-    pagination.classList.add(LIGHT);
-
+    body.classList.add(LIGHT);
     footer.classList.add(LIGHT);
 
-    gallery.classList.remove(DARK);
-    pagination.classList.remove(DARK);
-
+    body.classList.remove(DARK);
     footer.classList.remove(DARK);
 
     localStorage.setItem('theme', LIGHT);
@@ -49,9 +39,7 @@ if (!theme) {
   theme = LIGHT;
   localStorage.setItem('theme', theme);
 }
-gallery.classList.add(theme);
-pagination.classList.add(theme);
-
+body.classList.add(theme);
 footer.classList.add(theme);
 
 checkboxBtn.checked = theme === LIGHT ? false : true;
