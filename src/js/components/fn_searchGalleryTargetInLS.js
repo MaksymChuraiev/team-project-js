c47782f66e5fbb61eb0dd3c5471ce0935b6eb838
Michael Paukov <michael.paukov@gmail.com>
2022-02-04 08:11:26 +0100

    add teaser

--- src/js/components/fn_searchGalleryTargetInLS.js
+++ src/js/components/fn_searchGalleryTargetInLS.js
@@ -1,56 +1,48 @@
-export {getGalleryTargetMovieFromLS, currentFilm}
+export { getGalleryTargetMovieFromLS, currentFilm };
 
-
-
-import { cloneDeep } from 'lodash'
-import { modalWindowMarkup } from './fn_dataMovietoModal'
-import { data } from './library'
-
-
-
-
+import { cloneDeep } from 'lodash';
+import { modalWindowMarkup } from './fn_dataMovietoModal';
+import { data } from './library';
+import teaser from './teaser';
 
 const refs = {
-    gallery: document.querySelector('.gallery-list'),
-    modal: document.querySelector('.modal__wrapper'),
-    modalButtons: document.querySelector('.modal__buttons'),
-    
-}
+  gallery: document.querySelector('.gallery-list'),
+  modal: document.querySelector('.modal__wrapper'),
+  modalButtons: document.querySelector('.modal__buttons'),
+};
 // console.log(refs.gallery)
-console.log(refs.modal)
-let currentFilm 
-
+console.log(refs.modal);
+let currentFilm;
 
 function getGalleryTargetMovieFromLS() {
-    const lsKey = localStorage.getItem('isActive')
-    console.log(lsKey);
-    const galeryofFilms = [...refs.gallery.children]
-    let arrayofFilms
-    if (lsKey === 'home') {
-        arrayofFilms = JSON.parse(localStorage.getItem('MoviesOnPage'))
-        console.log('home',arrayofFilms);
-    }
-    if (lsKey === 'watched') {
-        arrayofFilms = JSON.parse(localStorage.getItem('watched'))
-        console.log('home',arrayofFilms);
-    }
-    if (lsKey === 'queue') {
-        arrayofFilms = JSON.parse(localStorage.getItem('queue'))
-        console.log('home',arrayofFilms);
-    }
-    // const arrayofFilms = JSON.parse(localStorage.getItem('MoviesOnPage'))
-    arrayofFilms.results.forEach((film, idxA) => {
-            // console.log(film, idxA)
-           galeryofFilms.forEach((li, idxB) => {
-                if (li === event.currentTarget) {
-                    if (idxA === idxB) {
-                        currentFilm = film
-                        // console.log('era', film)
-                        
-                        modalWindowMarkup(film)
-
-                    }
-                }
-           })
-    })
+  const lsKey = localStorage.getItem('isActive');
+  console.log(lsKey);
+  const galeryofFilms = [...refs.gallery.children];
+  let arrayofFilms;
+  if (lsKey === 'home') {
+    arrayofFilms = JSON.parse(localStorage.getItem('MoviesOnPage'));
+    console.log('home', arrayofFilms);
+  }
+  if (lsKey === 'watched') {
+    arrayofFilms = JSON.parse(localStorage.getItem('watched'));
+    console.log('home', arrayofFilms);
+  }
+  if (lsKey === 'queue') {
+    arrayofFilms = JSON.parse(localStorage.getItem('queue'));
+    console.log('home', arrayofFilms);
+  }
+  // const arrayofFilms = JSON.parse(localStorage.getItem('MoviesOnPage'))
+  arrayofFilms.results.forEach((film, idxA) => {
+    // console.log(film, idxA)
+    galeryofFilms.forEach((li, idxB) => {
+      if (li === event.currentTarget) {
+        if (idxA === idxB) {
+          currentFilm = film;
+          // console.log('era', film)
+          teaser(film.id);
+          modalWindowMarkup(film);
+        }
+      }
+    });
+  });
 }
